08a9e5e5eab1a83ddaafb6ecf0b7d70506f1a4da
Gerrit Großkopf (Holospace) <gerrit@familiegrosskopf.de>
2018-02-17 03:06:38 +0100

    updated
    now builds and runs, only the matrices never get updated

--- platform/uwp/os_uwp.cpp
+++ platform/uwp/os_uwp.cpp
@@ -199,8 +199,12 @@ Error OSUWP::initialize(const VideoMode &p_desired, int p_video_driver, int p_au
 
 	main_loop = NULL;
 	outside = true;
-
+    
+#ifdef HOLOGRAPHIC
+    gl_context->initializeHolo();
+#else
 	gl_context->initialize();
+#endif
 	VideoMode vm;
 	vm.width = gl_context->get_window_width();
 	vm.height = gl_context->get_window_height();
